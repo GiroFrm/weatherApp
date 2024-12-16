196b8d70fdd6bc3b01a719901aa91a58f9a4e186
GiroFrm <girofram844@gmail.com>
2024-12-16 15:56:29 +1100

    feat: Add fetchingWeather function to retrieve weather data by location

---
+++ src/api.js
@@ -0,0 +1,20 @@
+
+
+const API_KEY= 'A4TXWB2HDKTVSSGT775CEVL26'
+const unit ='uk'
+
+export default async function fetchingWeather(location) {
+  const API_CITY = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unit}&key=${API_KEY}&contentType=json`
+    try{
+    const response = await  fetch(API_CITY, {mode: 'cors'});
+    const data = await response.json();
+  
+    console.log(data);
+    console.log(data.currentConditions)
+    
+    }catch(Error){
+        console.log(Error);
+    }
+}
+
+
