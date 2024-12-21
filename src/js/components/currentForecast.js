1de69f5274e126a3c0207a81011ee9ac4febe66c
GiroFrm <girofram844@gmail.com>
2024-12-21 17:46:12 +1100

    Add functions to generate weather forecast elements
    - Implement `getCurrentForecast` to create forecast elements
    - Implement `getDescription` to add weather descriptions
    - Implement `getInfosCurrentWeather` to create detailed weather information elements
    - Add helper function `getElementInfo` for creating individual info elements

---
+++ src/js/components/currentForecast.js
@@ -0,0 +1,45 @@
+import { createElement, createImgElement } from "../../util";
+import { getConditionImg } from "./conditionImgs";
+
+export function getCurrentForecast(data){
+    
+    const tempContainer = createElement('div',"temp-container");
+    const imgConditions = getConditionImg(`${data.conditions}`);
+    const temperature = createElement('p', "temp-container__temp", `${data.temp}`);
+    const conditionsWeather = createElement('p', "temp_container__conditions",`${data.conditions}`);
+
+       tempContainer.appendChild(imgConditions);
+       tempContainer.appendChild(temperature);
+       tempContainer.appendChild(conditionsWeather);
+
+    return tempContainer
+}
+
+export function getDescription(description){
+    return createElement('p', " description ", `${description}`);
+}
+
+export function getInfosCurrentWeather(data) {
+   const containerInfos = createElement('div', "container-infos");
+   const feelsLike = getElementInfo("container-infos__feels","feels",data.feelslike);
+   const precipProb = getElementInfo("container-infos__precip", "precipitation" ,data.precipprob);
+   const wingSpeed = getElementInfo("container-infos__wind", 'wind' ,data.windspeed);
+   const humidity = getElementInfo("container-infos__hum", 'humidity' ,data.humidity);
+   const visibility = getElementInfo("container-infos__vis",'visibility' ,data.visibility);
+   const sunrise = getElementInfo("container-infos__sun",'sunrise' ,data.sunrise);
+   const sunset = getElementInfo("container-infos__sunset", 'sunset' , data.sunset);
+
+   containerInfos.append(feelsLike, precipProb, wingSpeed, humidity, visibility, sunrise, sunset );
+
+   return containerInfos;
+
+}
+
+function getElementInfo(className,name ,data,){
+    const span = createElement('span');
+    const infoElement = createElement('p',className, name);
+        span.innerHTML = `${data}`  
+        infoElement.appendChild(span);
+        return infoElement;
+}
+
