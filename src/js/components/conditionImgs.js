77969bb39f20d0a4b87b7da59d728d168f594139
GiroFrm <girofram844@gmail.com>
2024-12-21 17:52:37 +1100

    Update getConditionImg function to include width and height parameters
    - Added width and height parameters to dynamically set image dimensions
    - Updated switch cases to pass the new parameters to createImgElement

--- src/js/components/conditionImgs.js
+++ src/js/components/conditionImgs.js
@@ -8,23 +8,23 @@ import { createImgElement } from "../../util";
 
 
 
-export function getConditionImg(condition){
+export function getConditionImg(condition, width, height){
     switch(condition.toLowerCase()){
         case'rainy':
-           return  createImgElement(rain, "Rainy");
+           return  createImgElement(rain, "Rainy", width, height);
 
         case 'clear':
-           return createImgElement(sun, "Clear");
+           return createImgElement(sun, "Clear", width, height);
 
         case 'overcast':
-           return createImgElement(cloudy, "Overcast");
+           return createImgElement(cloudy, "Overcast", width, height);
         
         case 'snow':
-           return createImgElement(snow, "Snow");
+           return createImgElement(snow, "Snow", width, height);
 
         case "partially cloudy":
            default:
-              return createImgElement(partiallyCloudy, "Partially Cloudy");
+              return createImgElement(partiallyCloudy, "Partially Cloudy", width, height);
         
     } 
      
