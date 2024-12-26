b84dc81e160783c8ccad0a8213e3f77cf8478d4e
GiroFrm <girofram844@gmail.com>
2024-12-26 17:21:53 +1100

    refactor: Remove commented code

--- src/js/components/domRenderer.js
+++ src/js/components/domRenderer.js
@@ -5,34 +5,38 @@ import { getHourlyForecast } from './hourlyForecast.js';
 import { getCurrentForecast, getDescription, getInfosCurrentWeather } from './currentForecast.js';
 
 export default async function renderDom(location="melbourne") {
-       
-      const data = await fetchingWeather(location);
+      const wrapper_body =  document.querySelector('.wrapper');
       
-        console.log(data);  
-        console.log(typeof data.currentConditions.icon)
 
-        document.querySelector('.location').innerHTML= data.address;
-        document.querySelector('.current-time').innerHTML = getCurrentTime();
-
-      const currentContainer =  document.querySelector('.current-container');  
+       if (location) {
+          wrapper_body.innerHTML = `<p class="message">Loading...</p>`;
+     
+        const data = await fetchingWeather(location);
+      
+      wrapper_body.innerHTML = '';
+      const currentContainer =  createElement('div','current-container') //document.querySelector('.current-container');  
             currentContainer.innerHTML = '';
- 
+            wrapper_body.appendChild(currentContainer);
       const tempContainer =  getCurrentForecast(data.currentConditions);
          currentContainer.appendChild(tempContainer);
          currentContainer.appendChild(getDescription(`${data.description}`));
          currentContainer.appendChild(getInfosCurrentWeather(data.currentConditions));
 
-        const wrapper_body =  document.querySelector('.wrapper');
-       
         removeChildrenContainer("hourly-container");
 
         const hourly_container = createElement('div', 'hourly-container');
            wrapper_body.appendChild(getHourlyForecast(data, hourly_container));
            wrapper_body.appendChild(getDailyForecast(data, hourly_container));  
+
+       }
     }
+
+   
 
     document.querySelector('form').addEventListener('submit', handleFormSubmit);
 
+   
+   
     function handleFormSubmit(event) {
          event.preventDefault(); 
          const city = event.target.city.value;
