eb75d532f868f5d3eefcf07e980d43da06347ed7
GiroFrm <girofram844@gmail.com>
2024-12-28 16:04:08 +1100

    refactor: Refactored dailyForecast.js improve code formatting

--- src/js/components/dailyForecast.js
+++ src/js/components/dailyForecast.js
@@ -1,39 +1,39 @@
 //build daily component weather
 
 import { createElement, getDayName } from "../../util";
-import {getConditionImg } from "./conditionImgs"
+import { getConditionImg } from "./conditionImgs"
 
 export function getDailyForecast(data, hourly_container) {
-  
-    for (let i = 0; i < 7; i++) {
-        const dailyForecast = getForecastElement(data.days[i]);
-        hourly_container.appendChild(dailyForecast);
-    }
-    return hourly_container;
+
+  for (let i = 0; i < 7; i++) {
+    const dailyForecast = getForecastElement(data.days[i]);
+    hourly_container.appendChild(dailyForecast);
+  }
+  return hourly_container;
 }
 
 //build componet card-week-forecast
 function getForecastElement(data) {
   const conditions = data.conditions.split(",")[0];
-    const card_week_forecast = createElement('div', 'card-week-forecast');
-    const weekDay = getWeekDay(data.datetime);
-    const imgCondition = getConditionImg(conditions, '40', '40');
-    const temp_max = getTemperatureElement(data.tempmax, 'max');
-    const temp_min = getTemperatureElement(data.tempmin, 'min');
+  const card_week_forecast = createElement('div', 'card-week-forecast');
+  const weekDay = getWeekDay(data.datetime);
+  const imgCondition = getConditionImg(conditions, '40', '40');
+  const temp_max = getTemperatureElement(data.tempmax, 'max');
+  const temp_min = getTemperatureElement(data.tempmin, 'min');
 
-    card_week_forecast.appendChild(weekDay);
-    card_week_forecast.appendChild(imgCondition);
-    card_week_forecast.appendChild(temp_max);
-    card_week_forecast.appendChild(temp_min);
+  card_week_forecast.appendChild(weekDay);
+  card_week_forecast.appendChild(imgCondition);
+  card_week_forecast.appendChild(temp_max);
+  card_week_forecast.appendChild(temp_min);
 
-    return card_week_forecast;
+  return card_week_forecast;
 }
 
 function getWeekDay(datetime) {
-    const dayName = getDayName(datetime);
-    return createElement('p', 'card-week-forecast__day',`${dayName}`);
+  const dayName = getDayName(datetime);
+  return createElement('p', 'card-week-forecast__day', `${dayName}`);
 }
-      
+
 function getTemperatureElement(temp, type) {
   const className =
     type === "max"
@@ -42,6 +42,6 @@ function getTemperatureElement(temp, type) {
   return createElement("p", className, `${temp}°C`);
 }
 
-export function imgCondition(){
+export function imgCondition() {
   return getConditionImg('snow');
 } 
